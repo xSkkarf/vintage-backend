dc9d85c670839fbe70057b7a77eb7d8facbf51bf
xSkkarf <mostafa.ayyad.117@gmail.com>
2025-03-03 10:47:08 +0200

    Add token refresh

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from users.views import UserInfoView, RegisterUserView, LogInUserView, LogOutUserView
+from users.views import UserInfoView, RegisterUserView, LogInUserView, LogOutUserView, CookieTokenRefreshView
 
 urlpatterns = [
     path('user-info/', UserInfoView.as_view(), name='user-info'),
     path('register/', RegisterUserView.as_view(), name='user-register'),
     path('login/', LogInUserView.as_view(), name='user-login'),
     path('logout/', LogOutUserView.as_view(), name='user-logout'),
+    path('refresh/', CookieTokenRefreshView.as_view(), name='token-refresh'),
 ]
--- users/views.py
+++ users/views.py
@@ -7,6 +7,8 @@ from users.serializers import LogInUserSerializer, UserSerializer, RegisterUserS
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import status
+from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.exceptions import InvalidToken
 
 class UserInfoView(RetrieveUpdateAPIView):
     permission_classes = [IsAuthenticated]
@@ -52,11 +54,37 @@ class LogOutUserView(APIView):
                 refresh.blacklist()
             except Exception as e:
                 return Response({"message": "Error invalidating token" + str(e)},
-                                status=status.HTTP_400_BAD_REQUEST)
+                                status=status.HTTP_401_UNAUTHORIZED)
 
             response = Response({"message": "Logout successful"}, status=status.HTTP_200_OK)
             response.delete_cookie("access_token")
             response.delete_cookie("refresh_token")
             return response
         else:
-            return Response({"message": "No token provided"}, status=status.HTTP_400_BAD_REQUEST)  
+            return Response({"message": "No token provided"}, status=status.HTTP_401_UNAUTHORIZED)
+        
+class CookieTokenRefreshView(TokenRefreshView):
+    def post(self, request):
+        
+        refresh_token = request.COOKIES.get('refresh_token')
+        if not refresh_token:
+            return Response({"message": "No refresh token provided"}, status=status.HTTP_401_UNAUTHORIZED)
+        try:
+            refresh = RefreshToken(refresh_token)
+            access_token = str(refresh.access_token)
+            response = Response({"access_token set successfully"}, status=status.HTTP_200_OK)
+            response.set_cookie(key="access_token",
+                                value=access_token,
+                                httponly=True,
+                                secure=False,
+                                samesite='None')
+            response.set_cookie(key="refresh_token",
+                                value=str(refresh),
+                                httponly=True,
+                                secure=False,
+                                samesite='None')
+            
+            return response
+
+        except InvalidToken:
+            return Response({"message": "Token is invalid"}, status=status.HTTP_401_UNAUTHORIZED)
